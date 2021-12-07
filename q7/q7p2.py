b9417e7d9c86f042f8ffd4b9336ea6c18747bdab
Miles Steele <miles@milessteele.com>
2021-12-07 09:40:21 -0500

    q7p2 instant down from 20s

--- q7/q7p2.py
+++ q7/q7p2.py
@@ -2,38 +2,31 @@ import fileinput
 import numpy as np
 
 
-def memoize(function):
-    cache = {}
-
-    def decorated_function(*args):
-        if args in cache:
-            return cache[args]
-        else:
-            val = function(*args)
-            cache[args] = val
-            return val
-    return decorated_function
+def tri(size):
+    """triangular series: 0 1 3 6 10"""
+    return np.add.accumulate(np.arange(size))
 
 
-@memoize
-def tri(size): return np.fromfunction(
-    np.vectorize(lambda x: np.sum(np.arange(x+1))), (size,), dtype=int)
-
-
-@memoize
 def focus(size, x):
-    return np.concatenate([np.flip(tri(x+1)[1:]), tri(size-x)])
+    big = tri(size)
+    return np.concatenate([np.flip(tri(x+1)[1:]), big[:size-x]])
 
 
-positions = list(map(int, fileinput.input()[0].split(',')))
+positions = list(map(int, next(fileinput.input()).split(',')))
 print(f"{positions=}")
 crabs = np.zeros(max(positions)+1, dtype=int)
 for x in positions:
     crabs[x] += 1
 print(f"{crabs=}")
 print(f"{crabs.shape=}")
+width = crabs.shape[0]
+triw = tri(width)
+sled = np.concatenate([np.flip(triw)[:-1], triw])
+# print(f"{sled=}")
+# for x in range(crabs.shape[0]):
+#     print(x, width-x-1, width*2-x, sled[width-x-1:len(sled)-x])
 fuels = np.fromfunction(np.vectorize(lambda x: np.sum(
-    focus(crabs.shape[0], x)*crabs)), crabs.shape, dtype=int)
+    sled[width-x-1:len(sled)-x]*crabs)), crabs.shape, dtype=int)
 print(f"{fuels=}")
 print(np.argmin(fuels))
 print(min(fuels))
