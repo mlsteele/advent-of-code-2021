0f534b23aa302ce6d45eefdb35783e34850c0065
Miles Steele <miles@milessteele.com>
2021-12-10 07:45:21 -0500

    q10p1

---
+++ q10/q10p1.py
@@ -0,0 +1,21 @@
+import fileinput
+from functools import reduce
+PAIRS = {'(': ')', '{': '}', '[': ']', '<': '>', }
+START = set(PAIRS.keys())
+END = set(PAIRS.values())
+POINTS = {')': 3, ']': 57, '}': 1197, '>': 25137, None: 0, }
+
+
+def scan(line):
+    stack = []
+    for x in line:
+        if x in START:
+            stack.append(x)
+        else:
+            y = stack.pop()
+            if PAIRS[y] != x:
+                return x
+
+
+lines = [list(line.strip()) for line in fileinput.input()]
+print(sum([POINTS[scan(line)] for line in lines]))
