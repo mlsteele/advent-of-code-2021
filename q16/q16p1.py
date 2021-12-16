75a2c5091d48934e038a52ca3690642bfbd01b55
Miles Steele <miles@milessteele.com>
2021-12-16 08:07:32 -0500

    q16p1

--- q16/q16p1.py
+++ q16/q16p1.py
@@ -21,25 +21,46 @@ def shift(stream, n):
 def packet(stream):
     version = b2i(shift(stream, 3))
     type = b2i(shift(stream, 3))
-    if type != 4:
-        print("unrecognized packet type", type)
-        exit()
     if type == 4:
         # literal
         value = 0
         while True:
             value <<= 4
             chunk = shift(stream, 5)
-            print('chunk', chunk)
             value |= b2i(chunk[1:])
             if chunk[0] == 0:
                 break
+        print("-> ", (version, type, value))
         return (version, type, value)
-    return (version, type)
+    # operator
+    lti = shift(stream, 1)[0]
+    print("op", type, lti)
+    if lti == 0:
+        # next 15 bits are subpackets
+        subpacket_len = b2i(shift(stream, 15))
+        print("subp len", subpacket_len)
+        substream = deque(shift(stream, subpacket_len))
+        subpackets = []
+        while len(substream):
+            subpackets.append(packet(substream))
+        return (version, type, subpackets)
+    else:
+        subpacket_count = b2i(shift(stream, 11))
+        return (version, type, [packet(stream) for _ in range(subpacket_count)])
+
+
+def version_sum(ast):
+    res = ast[0]
+    if type(ast[2]) == list:
+        for sub in ast[2]:
+            res += version_sum(sub)
+    return res
 
 
 inhex = next(fileinput.input()).strip()
 stream = deque(map(int, ''.join([HEX[c] for c in inhex])))
 print(''.join(map(str, stream)))
-print(packet(stream))
-print(''.join(map(str, stream)))
+ast = packet(stream)
+print("remainder", ''.join(map(str, stream)))
+print(ast)
+print(version_sum(ast))
